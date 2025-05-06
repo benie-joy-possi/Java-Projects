ff3741d5f4b1500298091b404f77fb32b3793b02
benie-joy-possi <beniejoypossi@gmail.com>
2025-05-06 16:06:01 +0100

    Add ReverseInteger.java implementation for reversing an integer with overflow handling

---
+++ leetcode7/ReverseInteger.java
@@ -0,0 +1,31 @@
+package leetcode7;
+
+public class ReverseInteger {
+    public static int reverse(int x) {
+        int rev = 0;
+
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10) && pop > 7) {
+                return 0; // overflow
+            }
+
+            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) {
+                return 0; // Underflow
+            }
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse(123));   // Output: 321
+        System.out.println(reverse(-123));  // Output: -321
+        System.out.println(reverse(120));   // Output: 21
+        System.out.println(reverse(0));     // Output: 0
+    }
+
+
+}
